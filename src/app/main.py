ac69750d51ff80a309278c1e506e3bfd4737e28b
Proverbs Xu <379548839@qq.com>
2020-11-21 15:57:18 -0800

    add an endpoint for updating cache and fix an encoding issue

--- src/app/main.py
+++ src/app/main.py
@@ -3,39 +3,26 @@ from typing import Optional
 
 import psycopg2
 import redis
-from fastapi import FastAPI
+from fastapi import BackgroundTasks, FastAPI
 from pydantic import BaseModel
 
 
 app = FastAPI()
-cache = redis.Redis(host='redis', port=6379)
+cache = redis.StrictRedis(host='redis',
+                          port=6379,
+                          charset="utf-8",
+                          decode_responses=True)
 conn = psycopg2.connect(database=os.environ.get("POSTGRES_DB"),
                         user=os.environ.get("POSTGRES_USER"),
                         password=os.environ.get("POSTGRES_PASSWORD"),
                         host="db",
                         port="5432")
+conn.set_client_encoding('UTF8')
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
-
-@app.get("/cache")
-def read_root():
-    count = get_hit_count()
-    return {"cache_hit": count}
 
 
 def get_value_by_key_from_db(key: str):
@@ -91,10 +78,39 @@ def update_value_by_key(key: str, kv: KeyValue):
     return kv
 
 
+def update_all_key_values_in_cache():
+    # Updating all the cache usually take a long time, so I just want to use
+    # sleep to simulate a long task here.
+    import time
+    time.sleep(5)
+
+    keys = list(cache.scan_iter())
+    for key in keys:
+        value = get_value_by_key_from_db(key)
+        if not value:
+            cache.delete(key)
+        else:
+            cache.set(key, value)
+    return keys
+
+
 @app.post("/clear_cache")
 def clear_cache():
     count = 0
-    for key in cache.scan_iter():
+    keys = list(cache.scan_iter())
+    for key in keys:
         cache.delete(key)
         count += 1
-    return {"count": count}
+    return {"count": count, "keys": keys}
+
+
+@app.post("/async_update_cache")
+async def async_update_cache(background_tasks: BackgroundTasks):
+    background_tasks.add_task(update_all_key_values_in_cache)
+    return {"status": "updating cache in the background..."}
+
+
+@app.post("/update_cache")
+def update_cache():
+    keys = update_all_key_values_in_cache()
+    return {"count": len(keys), "keys": keys}
